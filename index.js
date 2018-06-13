40bd6537e91de05ccaaea678abfe4bee68b93681
Peter Cheng <chengtsztung.peter@gmail.com>
2018-06-14 00:21:36 +0800

    Init cli and interactive prompt

---
+++ index.js
@@ -0,0 +1,95 @@
+#!/usr/bin/env node
+
+const program = require('commander');
+const { prompt } = require('inquirer');
+const {
+  initRNWithProjectName,
+  cleanupProject,
+  modifiyEnvConfig,
+  runPodInstall,
+} = require('./rn-cli');
+const questions = [
+  {
+    type: 'input',
+    name: 'projectName',
+    message: 'Enter Project Name ...',
+    validate: input => {
+      if (input === '') {
+        return 'Project name cannot be empty.';
+      } else {
+        return true;
+      }
+    },
+  },
+  {
+    type: 'input',
+    name: 'skygearEndPointDevelopment',
+    message: 'Enter Skygear End Point (Development) ...',
+    default: 'http://127.0.0.1:3000/',
+  },
+  {
+    type: 'input',
+    name: 'skygearAPIKeyDevelopment',
+    message: 'Enter Skygear API Key (Development) ...',
+    default: answers => {
+      return `${answers.projectName.toLowerCase()}-development-api-key`;
+    },
+  },
+  {
+    type: 'input',
+    name: 'skygearEndPointStaging',
+    message: 'Enter Skygear End Point (Staging) ...',
+    default: answers => {
+      return `https://${answers.projectName.toLowerCase()}.staging.skygeario.com/`;
+    },
+  },
+  {
+    type: 'input',
+    name: 'skygearAPIKeyStaging',
+    message: 'Enter Skygear API Key (Staging) ...',
+  },
+  {
+    type: 'input',
+    name: 'skygearEndPointProduction',
+    message: 'Enter Skygear End Point (Production) ...',
+    default: answers => {
+      return `https://${answers.projectName.toLowerCase()}.skygeario.com/`;
+    },
+  },
+  {
+    type: 'input',
+    name: 'skygearAPIKeyProduction',
+    message: 'Enter Skygear API Key (Production) ...',
+  },
+  {
+    type: 'input',
+    name: 'sentryDSNDevelopment',
+    message: 'Enter Sentry DSN (Development) ...',
+  },
+  {
+    type: 'input',
+    name: 'sentryDSNStaging',
+    message: 'Enter Sentry DSN (Staging) ...',
+  },
+  {
+    type: 'input',
+    name: 'sentryDSNProduction',
+    message: 'Enter Sentry DSN (Production) ...',
+  },
+];
+
+program.version('0.0.1').description('React Native Skygear CLI');
+
+program
+  .command('init')
+  .description('init a react native project')
+  .action(() => {
+    prompt(questions).then(answers => {
+      initRNWithProjectName(answers.projectName);
+      cleanupProject(answers.projectName);
+      runPodInstall(answers.projectName);
+      modifiyEnvConfig(answers);
+    });
+  });
+
+program.parse(process.argv);
---
+++ rn-cli.js
@@ -0,0 +1,55 @@
+const execSync = require('child_process').execSync;
+const envfile = require('envfile');
+const fileUrl = require('file-url');
+const fs = require('fs');
+const path = require('path');
+
+//call react-native init <projectName> --template <template file path>
+const initRNWithProjectName = projectName => {
+  const templateFileURL = fileUrl(
+    path.dirname(require.resolve('react-native-template-oursky'))
+  );
+  execSync(`react-native init ${projectName} --template ${templateFileURL}`, {
+    stdio: 'inherit',
+  });
+};
+
+//call cleanup.js on generated project
+const cleanupProject = projectName => {
+  execSync(`cd ${projectName} && node cleanup.js`, {
+    stdio: 'inherit',
+  });
+};
+
+//inject skygear config and sentry config to .env files
+const modifiyEnvConfig = config => {
+  mergeEnvFile(path.resolve(config.projectName, 'app.env.development'), {
+    SKYGEAR_ENDPOINT: config.skygearEndPointDevelopment,
+    SKYGEAR_API_KEY: config.skygearAPIKeyDevelopment,
+    SENTRY_DSN: config.sentryDSNDevelopment,
+  });
+  mergeEnvFile(path.resolve(config.projectName, 'app.env.staging'), {
+    SKYGEAR_ENDPOINT: config.skygearEndPointStaging,
+    SKYGEAR_API_KEY: config.skygearAPIKeyStaging,
+    SENTRY_DSN: config.sentryDSNStaging,
+  });
+  mergeEnvFile(path.resolve(config.projectName, 'app.env.production'), {
+    SKYGEAR_ENDPOINT: config.skygearEndPointProduction,
+    SKYGEAR_API_KEY: config.skygearAPIKeyProduction,
+    SENTRY_DSN: config.sentryDSNProduction,
+  });
+};
+
+const mergeEnvFile = (path, config) => {
+  const oldEnv = envfile.parseFileSync(path);
+  const newEnv = Object.assign(oldEnv, config);
+  fs.writeFileSync(path, envfile.stringifySync(newEnv));
+};
+
+const runPodInstall = projectName => {
+  execSync(`cd ${projectName}/ios && pod install`, {
+    stdio: 'inherit',
+  });
+};
+
+module.exports = { initRNWithProjectName, cleanupProject, modifiyEnvConfig, runPodInstall};
