eaec90d9bf4258792d97e157ea277bde9b15bcdd
Peter Cheng <chengtsztung.peter@gmail.com>
2018-06-14 11:58:29 +0800

    Setup Skygear at JS

--- template/react-native-template-oursky/index.js
+++ template/react-native-template-oursky/index.js
@@ -1,7 +1,22 @@
 import { AppRegistry } from 'react-native';
 import Raven from 'raven-js';
-import Config from 'react-native-config'
+import Config from 'react-native-config';
+import skygear from 'skygear';
 import App from './js/App';
 
-AppRegistry.registerComponent('HelloWorld', () => App);
+AppRegistry.registerComponent('TemplateTest', () => App);
 Raven.config(Config.SENTRY_DSN).install();
+
+skygear
+  .config({
+    endPoint: Config.SKYGEAR_ENDPOINT,
+    apiKey: Config.SKYGEAR_API_KEY,
+  })
+  .then(
+    () => {
+      console.log('skygear container is now ready for making API calls.');
+    },
+    error => {
+      console.error(error);
+    }
+  );
