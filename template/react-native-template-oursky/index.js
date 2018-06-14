0a50379928d51255c9b781cd57ddcf05ad8db82a
Peter Cheng <chengtsztung.peter@gmail.com>
2018-06-14 11:33:57 +0800

    Setup Sentry at JS

--- template/react-native-template-oursky/index.js
+++ template/react-native-template-oursky/index.js
@@ -1,4 +1,7 @@
 import { AppRegistry } from 'react-native';
+import Raven from 'raven-js';
+import Config from 'react-native-config'
 import App from './js/App';
 
 AppRegistry.registerComponent('HelloWorld', () => App);
+Raven.config(Config.SENTRY_DSN).install();
