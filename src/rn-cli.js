333ef0f8ac7eb5b044a54a77847bec850538dc4d
Peter Cheng <chengtsztung.peter@gmail.com>
2018-06-20 12:15:10 +0800

    Rename template to templates

--- src/rn-cli.js
+++ src/rn-cli.js
@@ -11,7 +11,7 @@ const initRNWithProjectName = projectName => {
     path.resolve(
       __dirname,
       '..',
-      'template',
+      'templates',
       'react-native-template-oursky'
     )
   );
