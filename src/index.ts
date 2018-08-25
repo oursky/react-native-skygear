cba5d23b4e7f97aba8ba4308efd21f8f648a05ba
YinYin Chiu <cychiuae@connect.ust.hk>
2018-08-25 16:54:02 +0800

    Install prettier

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
-import * as program from 'commander';
+import * as program from "commander";
 
 program
-  .version('2.0.0')
-  .description('Create React Native project with Skygear')
+  .version("2.0.0")
+  .description("Create React Native project with Skygear");
 
 program.parse(process.argv);
 if (program.args.length === 0) {
-  console.error("You didn't pass any command\nSee --help for a list of available commands.");
+  console.error(
+    "You didn't pass any command\nSee --help for a list of available commands."
+  );
 }
