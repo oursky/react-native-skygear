bfe39bc59580f8e6ebbf8b53a43ad00dfb1529f1
YinYin Chiu <cychiuae@connect.ust.hk>
2018-08-25 16:45:02 +0800

    Create program

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,10 @@
-function sayHello(name: string) {
-  console.log(`Hello ${name}`);
-}
+import * as program from 'commander';
 
-sayHello('World');
+program
+  .version('2.0.0')
+  .description('Create React Native project with Skygear')
+
+program.parse(process.argv);
+if (program.args.length === 0) {
+  console.error("You didn't pass any command\nSee --help for a list of available commands.");
+}
