7aa20f2460684f51e4e70fe2075a2903919a35e8
YinYin Chiu <cychiuae@connect.ust.hk>
2018-08-25 17:48:50 +0800

    Print help

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,17 @@
 import * as program from "commander";
 
+import { registerCommand as registerHelpCommand } from "./commands/help";
+
 program
   .version("2.0.0")
   .description("Create React Native project with Skygear");
+
+registerHelpCommand(program);
+
+program.on("command:*", () => {
+  console.error("Invalid command: %s", program.args.join(" "));
+  program.help();
+});
 
 program.parse(process.argv);
 if (program.args.length === 0) {
