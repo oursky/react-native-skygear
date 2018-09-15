70474dc9973891e19b0033d5a99535db58720a17
YinYin Chiu <cychiuae@connect.ust.hk>
2018-09-15 17:15:59 +0800

    Install react native sentry

--- templates/react-native-skygear/android/app/src/main/java/com/helloworld/MainApplication.java
+++ templates/react-native-skygear/android/app/src/main/java/com/helloworld/MainApplication.java
@@ -9,6 +9,8 @@ import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
 import com.lugg.ReactNativeConfig.ReactNativeConfigPackage;
 
+import io.sentry.RNSentryPackage;
+
 import java.util.Arrays;
 import java.util.List;
 
@@ -24,6 +26,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new ReactNativeConfigPackage(),
+          new RNSentryPackage(),
           new MainReactPackage()
       );
     }
