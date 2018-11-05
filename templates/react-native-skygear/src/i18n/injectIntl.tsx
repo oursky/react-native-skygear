8618a064676d2c92c60792da628d36fddca7dce4
YinYin Chiu <cychiuae@connect.ust.hk>
2018-11-05 18:32:15 +0800

    Add injectIntl to template

---
+++ templates/react-native-skygear/src/i18n/injectIntl.tsx
@@ -0,0 +1,26 @@
+import * as React from "react";
+import { Consumer, ContextValue } from "@oursky/react-messageformat";
+import hoistNonReactStatic from "hoist-non-react-statics";
+
+type Omit<T, K> = Pick<T, Exclude<keyof T, K>>;
+type Subtract<T, K> = Omit<T, keyof K>;
+
+export interface IntlProps {
+  intl: ContextValue;
+}
+
+function _injectIntl<Props extends IntlProps>(
+  Component: React.ComponentType<Props>
+) {
+  return function(props: Subtract<Props, IntlProps>) {
+    return <Consumer>{intl => <Component {...props} intl={intl} />}</Consumer>;
+  };
+}
+
+export function injectIntl<Props extends IntlProps>(
+  Component: React.ComponentType<Props>
+) {
+  const WrappedComponent = _injectIntl(Component);
+  hoistNonReactStatic(WrappedComponent, Component);
+  return WrappedComponent;
+}
