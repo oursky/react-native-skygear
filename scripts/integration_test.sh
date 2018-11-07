78f9783302ada984fb74a94b882e060b7c99e0b7
YinYin Chiu <cychiuae@connect.ust.hk>
2018-11-07 18:24:37 +0800

    Fix integration test

--- scripts/integration_test.sh
+++ scripts/integration_test.sh
@@ -1,13 +1,24 @@
-#!/bin/sh -e
+#!/bin/bash -e
+
+PROJECT_DIR="$HOME/create-react-native-skygear-integration"
+PROJECT_NAME=HelloWorld
 
 yarn build
-./bin/index.js init --helloworld HelloWorld
-mv HelloWorld /tmp
-pushd /tmp/HelloWorld
+./bin/index.js init --helloworld "$PROJECT_NAME"
+rm -rf "$PROJECT_DIR/$PROJECT_NAME"
+mkdir -p "$PROJECT_DIR"
+mv "$PROJECT_NAME" "$PROJECT_DIR"
+pushd "$PROJECT_DIR/$PROJECT_NAME"
 yarn run-ci
 if [ "$1" = "ios" ]; then
   react-native run-ios
 elif [ "$1" = "android" ]; then
-  react-native run-android
+  pushd android
+  ./gradlew tasks
+  ./gradlew assembleDevelopmentDebug
+  popd
+  mkdir -p output/res
+  react-native bundle --platform android --dev false --entry-file index.js --bundle-output ./output/index.android.bundle --assets-dest ./output/res/
 fi
+rm -rf "$PROJECT_DIR"
 popd
