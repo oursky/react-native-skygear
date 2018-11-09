0b7c60e09d65672ac1a5efe4d3dcf394d5ca6af5
YinYin Chiu <cychiuae@connect.ust.hk>
2018-11-09 11:25:28 +0800

    Add Maybe type

---
+++ src/types.ts
@@ -0,0 +1,25 @@
+class Maybe<T> {
+  value?: T;
+
+  private constructor(value?: T) {
+    this.value = value;
+  }
+
+  static some<T>(value: T) {
+    if (!value) {
+      new Error("You must provide a value");
+    }
+    return new Maybe(value);
+  }
+
+  static none<T>() {
+    return new Maybe<T>();
+  }
+
+  map<R>(f: (a: T) => R): Maybe<R> {
+    if (this.value == null) {
+      return Maybe.none<R>();
+    }
+    return Maybe.some<R>(f(this.value));
+  }
+}
